5e297d79438334b2f966035f74517dcd20060daa
wxkdesky <wxkdesky@hotmail.com>
2015-06-17 22:39:09 +0800

    最后的绑定出现问题

--- waimai/DetailRest.xaml.cs
+++ waimai/DetailRest.xaml.cs
@@ -18,6 +18,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
+using waimai.BingdingClass;
+using Windows.UI.Xaml.Media.Imaging;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
@@ -49,12 +51,21 @@ namespace waimai
         /// <param name="e">描述如何访问此页的事件数据。
         /// 此参数通常用于配置页。</param>
         string[] nameToCopy = new string[2] { "",""};
+        SpecificNRest wholeRest;
+        List<nFood> foodCollection;
+        nFood foodOne;
         protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             //name for url and restname
             string[]nameTo = (string[])e.Parameter;
             if (nameTo[0] != nameToCopy[0])
             {
+                //initialize
+                wholeRest=new SpecificNRest();
+                wholeRest.itemName = new List<string>();
+                wholeRest.itemDescription = new List<string>();
+                foodCollection=new List<nFood>();
+                foodOne=new nFood();
                 restNameTb.Text = nameTo[1];
                 msg = new HttpResponseMessage();
                 string add = "http://restapi.ele.me/v1/restaurants/" + nameTo[0] + "/menu?full_image_path=1";
@@ -75,36 +86,88 @@ namespace waimai
                     ToastNotificationManager.CreateToastNotifier().Show(toast);
                 }
                 Popup p = new Popup();
-                try
-                {
+               // try
+                //{
                     // responseText = "{" + responseText + "}";
                     myArray = JsonArray.Parse(responseText);
-                    //myArray = jsonObject.GetArray();
-                }
-                catch
-                {
-                    TextBlock tb = new TextBlock();
-                    tb.Text = "数据解析发生未知错误";
-                    tb.FontSize = 20;
-                    tb.VerticalAlignment = VerticalAlignment.Center;
-                    tb.HorizontalAlignment = HorizontalAlignment.Center;
-                    StackPanel Panel = new StackPanel();
-                    Panel.Background = new SolidColorBrush(Windows.UI.Colors.Cyan);
-                    Panel.HorizontalAlignment = HorizontalAlignment.Center;
-                    Panel.VerticalAlignment = VerticalAlignment.Center;
-                    Panel.Height = 40;
-                    Panel.Width = 400;
-                    Panel.Children.Add(tb);
-                    p.Child = Panel;
-                    p.IsOpen = true;
-                }
-                await Task.Delay(2000);
-                p.IsOpen = false;
-                foreach (var item in myArray)
-                {
-                    JsonObject temp = item.GetObject();
-                    //tb1.Text += temp["description"].GetString();
-                }
+                    foreach (var item in myArray)
+                    {
+                        JsonObject temp = item.GetObject();
+                        wholeRest.itemName.Add(temp["name"].GetString());
+                        wholeRest.itemDescription.Add(temp["description"].GetString());
+                        var foodArray = temp["foods"].GetArray();
+                        foreach (var one in foodArray)
+                        {
+                            JsonObject jso = one.GetObject();
+                            foodOne.foodDescription = jso["description"].GetString();
+                            foodOne.foodName = jso["name"].GetString();
+                            if (jso["image_path"].GetString()=="")
+                                foodOne.foodImage = new BitmapImage(new Uri("ms-appx:///Assets/defaultPic.jpg"));
+                            else
+                                foodOne.foodImage = new BitmapImage(new Uri(jso["image_path"].GetString()));
+                            foodOne.monthSale =Convert.ToString( jso["month_sales"].GetNumber());
+                            foodOne.foodPrice = Convert.ToString(jso["price"].GetNumber());
+                            foodOne.Evaluate = Convert.ToString(jso["rating_count"].GetNumber()) + "评价";
+                            double rate = jso["rating"].GetNumber();
+                            if (rate > 4)
+                                foodOne.rateStar = "★★★★★";
+                            else if (rate > 3)
+                                foodOne.rateStar = "★★★★☆";
+                            else if (rate > 2)
+                                foodOne.rateStar = "★★★☆☆";
+                            else if (rate > 1)
+                                foodOne.rateStar = "★★☆☆☆";
+                            else
+                                foodOne.rateStar = "★☆☆☆☆";
+                           // foodOne.foodActivity = jso["activity"].GetString();
+                            var attribute = jso["attributes"].GetArray();
+                            for(int i=0;i<attribute.Count;i++)
+                            {
+                                JsonObject jso2 = attribute[i].GetObject();
+                                if (i == 0)
+                                    foodOne.foodAttributes1 = jso2["icon_name"].GetString();
+                                else
+                                    foodOne.foodAttributes2 = jso2["icon_name"].GetString();                                
+                            }
+                            try
+                            {
+                                var limit = jso["limitation"].GetObject();
+                                foodOne.foodLimitation = limit["text"].GetString();
+                            }
+                            catch
+                            {
+                                foodOne.foodLimitation = "";
+                            }
+                        }
+                        foodCollection.Add(foodOne);
+                    }
+                    wholeRest.allfoods = foodCollection;
+                    //默认绑定第一个条目
+                    itemList.ItemsSource = wholeRest.itemName;
+                    topDescription td=new topDescription(){itemDescription=wholeRest.itemName[0],itemName=wholeRest.itemDescription[0]};
+                    topDescription.DataContext = td;
+                    foodList.ItemsSource = wholeRest.allfoods[0];
+                //}
+                //catch
+                //{
+                //    TextBlock tb = new TextBlock();
+                //    tb.Text = "数据解析发生未知错误";
+                //    tb.FontSize = 20;
+                //    tb.VerticalAlignment = VerticalAlignment.Center;
+                //    tb.HorizontalAlignment = HorizontalAlignment.Center;
+                //    StackPanel Panel = new StackPanel();
+                //    Panel.Background = new SolidColorBrush(Windows.UI.Colors.Cyan);
+                //    Panel.HorizontalAlignment = HorizontalAlignment.Center;
+                //    Panel.VerticalAlignment = VerticalAlignment.Center;
+                //    Panel.Height = 40;
+                //    Panel.Width = 400;
+                //    Panel.Children.Add(tb);
+                //    p.Child = Panel;
+                //    p.IsOpen = true;
+                //}
+                //await Task.Delay(2000);
+                //p.IsOpen = false;
+                
                 nameToCopy = nameTo;
             }
             else
@@ -123,6 +186,11 @@ namespace waimai
         {
             if (Frame.CanGoBack)
                 this.Frame.GoBack();//Navigate(typeof(MainPage), 1);
+        }
+
+        private void itemList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Debug.WriteLine(((ListViewItem)e.ClickedItem).TabIndex);
         }
     }
 }
